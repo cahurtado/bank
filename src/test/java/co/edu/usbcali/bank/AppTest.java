e4f9f254b122399c9c823261a0c162e829e47545
cahurtado <camilo323@hotmail.com>
2020-05-26 19:45:13 -0500

    Prueba de nuevo

---
+++ src/test/java/co/edu/usbcali/bank/AppTest.java
@@ -0,0 +1,20 @@
+package co.edu.usbcali.bank;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+}
